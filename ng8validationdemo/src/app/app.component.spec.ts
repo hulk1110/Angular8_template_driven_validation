1ce857f6a9a8ecd9b984cfd92ee19aee21a08f3c
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-11-18 19:52:29 +0530

    working code

--- ng8validationdemo/src/app/app.component.spec.ts
+++ ng8validationdemo/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng8validationdemo'`, () => {
+  it(`should have as title 'template-driven-form-validation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng8validationdemo');
+    expect(app.title).toEqual('template-driven-form-validation');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng8validationdemo app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to template-driven-form-validation!');
   });
 });
--- ng8validationdemo/src/app/app.module.ts
+++ ng8validationdemo/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ConfirmEqualValidatorDirective } from './confirm-equal-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ConfirmEqualValidatorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
